c20a512e80c0bb283965ec123e7d9f9deab32c59
David <garcialfa19@icloud.com>
2020-12-09 16:24:02 -0600

    David.Money is finished

--- src/David/Money/Tester.java
+++ src/David/Money/Tester.java
@@ -19,7 +19,7 @@ public class Tester {
             int numCurrency = s.nextInt();
             System.out.println("Please enter the Target Currency 1=Colon  2=Dollar  3=Euro");
             int numTarget = s.nextInt();
-            System.out.println("Please enter the amount");
+            System.out.println("Please enter the amount: ");
             int amount = s.nextInt();
 
             if (numCurrency == 1) { //colon
@@ -31,9 +31,21 @@ public class Tester {
                     System.out.println("The equivalent value is: " + mon.convert(cColon, cEuro, amount));
                 }
             } else if (numCurrency == 2) { // dollar
-
-            } else { // euro
-
+                if (numTarget == 1) { //colon
+                    System.out.println("The equivalent value is: " + mon.convert(cDollar, cColon, amount));
+                } else if (numTarget == 2) { // Dollar
+                    System.out.println("The equivalent value is: " + mon.convert(cDollar, cDollar, amount));
+                } else { // euro
+                    System.out.println("The equivalent value is: " + mon.convert(cDollar, cEuro, amount));
+                }
+            } else if (numCurrency ==3){ // euro
+                if (numTarget == 1) { //colon
+                    System.out.println("The equivalent value is: " + mon.convert(cEuro, cColon, amount));
+                } else if (numTarget == 2) { // Dollar
+                    System.out.println("The equivalent value is: " + mon.convert(cEuro, cDollar, amount));
+                } else { // euro
+                    System.out.println("The equivalent value is: " + mon.convert(cEuro, cEuro, amount));
+                }
             }
         }
     }
