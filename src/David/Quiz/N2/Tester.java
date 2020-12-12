3eee09a31317ddd55f322d8c7bf0945ea4bc868d
David <garcialfa19@icloud.com>
2020-12-11 22:05:26 -0600

    Quiz is Added

---
+++ src/David/Quiz/N2/Tester.java
@@ -0,0 +1,32 @@
+package David.Quiz.N2;
+
+public class Tester {
+
+    public static void main(String[] args) {
+
+        String name = "Pepperoni";
+        int code = 1;
+        String description = "The most delicious pizza topping";
+        int price = 1000;
+        Ingredient pepperoni = new Ingredient(name, code, description, price);
+
+
+        name = "Cheese";
+        code = 2;
+        description = "A must have on any pizza";
+        price = 500;
+        Ingredient cheese = new Ingredient(name, code, description, price);
+
+        name = "Mushrooms";
+        code = 2;
+        description = "A perfect match for Pepperoni";
+        price = 1000;
+        Ingredient mushrooms = new Ingredient(name, code, description, price);
+
+        int tprice = 3250;
+
+        Pizza p = new Pizza(pepperoni, cheese, mushrooms, tprice);
+
+
+    }
+}
